93a83d9a2732497667797825730a010f40c6f641
TiagoSenaD <50551963+TiagoSenaD@users.noreply.github.com>
2025-12-16 15:51:18 -0300

    creating Footer for my blog

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="flex justify-between items-center p-4 bg-gray-900 text-white shadow-md">
+                <p>© 2025 Blog Next. Todos os direitos reservados.</p>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
